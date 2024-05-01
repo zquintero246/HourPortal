bc7a1745b033e4278ab055c21ed30d1399a92134
Zabdiel Julian <128274532+zquintero246@users.noreply.github.com>
2024-05-01 17:32:17 -0500

    Add files via upload

---
+++ HourPortal/src/main/java/com/example/HourPortal/HourPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.HourPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourPortalApplication.class, args);
+	}
+
+}
