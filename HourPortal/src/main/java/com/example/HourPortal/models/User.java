5b2e380da2c4abe6f961c55f912cbf0496078a23
Zabdiel Julian <128274532+zquintero246@users.noreply.github.com>
2024-05-02 01:40:17 -0500

    update

--- HourPortal/src/main/java/com/example/HourPortal/models/User.java
+++ HourPortal/src/main/java/com/example/HourPortal/models/User.java
@@ -1,28 +1,33 @@
-package com.example.HourPortal.models;
+    package com.example.HourPortal.models;
 
 
-import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "users")
-@Data
-public class User {
-
-    @Id
-    private String id;
+    import lombok.Data;
+    import org.springframework.data.annotation.Id;
+    import org.springframework.data.mongodb.core.mapping.Document;
 
 
-    private String nombre;
-    private String semestre;
-    private String carrera;
+    //Se usa la anotacion document de mongodb para relacionarlo con la collection users en la base de datos
+    @Document(collection = "users")
+
+    //Anotacion de lombok para ahorrar los getters y setters del modelo
+    @Data
+    public class User {
+
+        //Se genera un id en la base de datos automatico
+        @Id
+        private String id;
+
+
+            private String nombre;
+            private String semestre;
+            private String carrera;
 
 
 
-    private String correoUnab;
-    private String contraseña;
-    private String idUnab;
+            private String correoUnab;
+            private String contraseña;
+            private String idUnab;
 
 
-    private int cantHoras;
-}
+            private int cantHoras;
+    }
---
+++ HourPortal/src/main/java/com/example/HourPortal/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.HourPortal.services;
+
+import com.example.HourPortal.models.User;
+import com.example.HourPortal.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//Anotacion de tipo servicio de spring
+@Service
+@Slf4j
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    //Importa el repositorio de usuarios para poder acceder a el y manejar las solicitudes
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    //Metodo que crea un nuevo usuario en la base de datos
+    public void registroUser(User user) {
+        log.info("Se ha registrado un usuario correctamente!");
+        userRepository.save(user);
+    }
+}
