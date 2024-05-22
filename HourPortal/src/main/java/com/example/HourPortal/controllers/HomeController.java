e406899b8e2b7b1683bce8a8376dda0ca7a697b7
Zabdiel Julian <128274532+zquintero246@users.noreply.github.com>
2024-05-22 01:39:40 -0500

    update

--- HourPortal/src/main/java/com/example/HourPortal/controllers/HomeController.java
+++ HourPortal/src/main/java/com/example/HourPortal/controllers/HomeController.java
@@ -22,5 +22,36 @@ public class HomeController {
         return "Home";
     }
 
+    @GetMapping("/Horas")
+    public String horas(Model model){
+
+        log.info("Se estan ejecutando las horas ");
+
+        return "Horas";
+    }
+
+    @GetMapping("/Eventos")
+    public String eventos(Model model){
+
+        log.info("Se estan ejecutando eventos ");
+
+        return "Eventos";
+    }
+    @GetMapping("/Ayuda")
+    public String ayuda(Model model){
+
+        log.info("Se esta ejecutando ayuda ");
+
+        return "Ayuda";
+    }
+    @GetMapping("/Noticias")
+    public String noticias(Model model){
+
+        log.info("Se estan ejecutando las noticias ");
+
+        return "Noticias";
+    }
+
+
 
 }
